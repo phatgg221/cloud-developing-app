0753393ab2c92a6bf337bce095e930abf3fe83d2
phatgg221 <129394719+phatgg221@users.noreply.github.com>
2024-12-07 15:03:15 +0700

    update: About us component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,15 @@
 
 import Header from '../components/application_component/Header';
 import Hero from '@/components/application_component/Hero';
+import React from 'react';
+import IntroductionComponent from '@/components/application_component/IntroductionComponent';
 export default function Home() {
   return (
     <div>
       {/* <Header/> */}
 
       <Hero></Hero>
+      <IntroductionComponent></IntroductionComponent>
     </div>
   );
 }
--- src/components/application_component/IntroductionComponent.jsx
+++ src/components/application_component/IntroductionComponent.jsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 
 const Intro = () => {
   return (
-    <div className="flex flex-col lg:flex-row pt-[50px] px-[20px] lg:px-[220px]">
+    <div className="flex flex-col pb-11 lg:flex-row pt-[50px] px-[20px] lg:px-[220px]">
       <div className="flex-1 max-w-full lg:max-w-md mb-8 lg:mb-0">
         <Image
-          src={"/beefSteak.png"}
+          src={"/cafe-hero.jpg"}
           width={420}
           height={416}
           alt="Beef"
@@ -18,13 +18,13 @@ const Intro = () => {
           Who Are We?
         </p>
         <p className="text-base md:text-lg text-gray-700">
-        The mission is to deliver unique food at affordable prices and we are the people to do it. 
+        The mission is to deliver unique cafe at affordable prices and we are the people to do it. 
         </p>
         <p className="text-2xl pt-[30px] md:text-3xl font-bold text-black mb-4">
         What makes us unique?
         </p>
         <p className="text-base md:text-lg text-gray-700">
-        Our menu is developed by high level chefs to deliver the best of both worlds without any compromises. 
+        Our menu is developed by high level chefs and we are the only cafe in the area that offers a unique experience.
         </p>
       </div>
     </div>
