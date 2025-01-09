7878e7c91eed63e58cd801fe05a6ee3b79cfeabf
phatgg221 <129394719+phatgg221@users.noreply.github.com>
2025-01-10 03:29:44 +0700

    Update page.jsx

--- src/app/admin/manage-menu/page.jsx
+++ src/app/admin/manage-menu/page.jsx
@@ -14,52 +14,54 @@ import { useRouter } from "next/navigation";
 
 const MenuTable = () => {
   const [data, setData] = useState([]);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
   const router = useRouter();
-  const [user, setUser]= useState(null);
-  if(user && !user?.isAdmin){
-    router.push('/');
-  }
-  console.log(user)
-    useEffect(() => {
-       const fetchUserInfo = async () => {
-         try {
-           const response = await fetch("/api/me");
-           if (response.ok) {
-             const userInfo = await response.json();
-             setUser(userInfo.userInfo);
-   
-             const token = document.cookie
-               .split("; ")
-               .find((row) => row.startsWith("accessToken="))
-               ?.split("=")[1];
-             setAccessToken(token);
-           } else {
-            setUser(null);
-           }
-         } catch (error) {
-           console.error("Error fetching user info:", error);
-         }
-       };
-   
-       fetchUserInfo();
-     }, []);
 
+  // Redirect non-admin users
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const response = await fetch("/api/me");
+        if (response.ok) {
+          const userInfo = await response.json();
+          setUser(userInfo.userInfo);
+
+          if (!userInfo.userInfo?.isAdmin) {
+            router.push("/");
+          }
+        } else {
+          setUser(null);
+          router.push("/");
+        }
+      } catch (error) {
+        console.error("Error fetching user info:", error);
+        router.push("/");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUserInfo();
+  }, [router]);
+
+  // Fetch menu data
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(
           "https://ic1ln5cze5.execute-api.us-east-1.amazonaws.com/MenuStage/getMenu"
         );
+
         if (response.ok) {
-          const data = await response.json();
-          const body = JSON.parse(data.body);
-          console.log(body.data);
+          const result = await response.json();
+          const body = JSON.parse(result.body);
           setData(body.data);
         } else {
-          console.error("Failed to fetch data");
+          console.error("Failed to fetch menu data");
         }
       } catch (error) {
-        console.error("Error fetching data:", error);
+        console.error("Error fetching menu data:", error);
       }
     };
 
@@ -67,29 +69,26 @@ const MenuTable = () => {
   }, []);
 
   const handleDelete = async (menu) => {
-    const requestBody = {
-        httpMethod: "DELETE",
-        body: JSON.stringify({
-          id: menu.id,
-        }),
-      };
     try {
-      const response = await fetch(`https://ic1ln5cze5.execute-api.us-east-1.amazonaws.com/MenuStage/deleteMenu`, {
-        method: "DELETE",
-        headers: {
+      const response = await fetch(
+        `https://ic1ln5cze5.execute-api.us-east-1.amazonaws.com/MenuStage/deleteMenu`,
+        {
+          method: "DELETE",
+          headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify(requestBody),
-      });
+          body: JSON.stringify({ id: menu.id }),
+        }
+      );
 
       if (response.ok) {
-        console.log("Menu deleted successfully");
         setData((prevData) => prevData.filter((item) => item.id !== menu.id));
+        console.log("Menu deleted successfully");
       } else {
         console.error("Failed to delete menu");
       }
     } catch (error) {
-      console.error("Error deleting menu", error);
+      console.error("Error deleting menu:", error);
     }
   };
 
@@ -101,12 +100,11 @@ const MenuTable = () => {
     router.push("/admin/manage-menu/form");
   };
 
+  const handleUpdate = (menu) => {
+    router.push(`/admin/manage-menu/form?id=${menu.id}`);
+  };
 
-    const handleUpdate = (menu) => {
-        router.push(`/admin/manage-menu/form?id=${menu.id}`);
-      };
-      
-
+  if (loading) return <div>Loading...</div>;
 
   return (
     <>
@@ -120,64 +118,61 @@ const MenuTable = () => {
             </tr>
           </thead>
           <tbody>
-            {data &&
-              data.map((menu) => (
-                <tr key={menu.id} className={style.tableRow}>
-                  <td>{menu.title}</td>
-                  <td className={style.btnContainer}>
-                    {menu.dishes &&
-                      menu.dishes.map((dish, index) => (
-                        <Dialog key={`${menu.id}-${index}`}>
-                          <DialogTrigger asChild>
-                            <button
-                              className={`${style.btn} ${style.btnTable}`}
-                            >
-                              {dish.name}
-                            </button>
-                          </DialogTrigger>
-                          <DialogContent>
-                            <DialogHeader>
-                              <DialogTitle>{dish.name}</DialogTitle>
-                              <DialogDescription>
-                                {dish.description}
-                              </DialogDescription>
-                            </DialogHeader>
-                            <div className="mt-4">
-                                {/* eslint-disable-next-line @next/next/no-img-element */}
-                              <img
-                                src={dish.image}
-                                alt={dish.name}
-                                className="w-full h-auto rounded-md mb-4"
-                              />
-                              <p className="text-gray-700">
-                                Price:{" "}
-                                <span className="text-yellow-500">
-                                  {dish.price}
-                                </span>
-                              </p>
-                            </div>
-                          </DialogContent>
-                        </Dialog>
-                      ))}
-                  </td>
-                  <td className={style.btnContainer}>
-                    <div className={style.btnTableDiv}>
-                      <button
-                        className={`${style.btn} ${style.btnTable}`}
-                        onClick={() => handleUpdate(menu)}
-                      >
-                        Update
-                      </button>
-                      <button
-                        className={`${style.btn} ${style.btnTable}`}
-                        onClick={() => handleDelete(menu)}
-                      >
-                        Delete
-                      </button>
-                    </div>
-                  </td>
-                </tr>
-              ))}
+            {data.map((menu) => (
+              <tr key={menu.id} className={style.tableRow}>
+                <td>{menu.title}</td>
+                <td className={style.btnContainer}>
+                  {menu.dishes &&
+                    menu.dishes.map((dish, index) => (
+                      <Dialog key={`${menu.id}-${index}`}>
+                        <DialogTrigger asChild>
+                          <button className={`${style.btn} ${style.btnTable}`}>
+                            {dish.name}
+                          </button>
+                        </DialogTrigger>
+                        <DialogContent>
+                          <DialogHeader>
+                            <DialogTitle>{dish.name}</DialogTitle>
+                            <DialogDescription>
+                              {dish.description}
+                            </DialogDescription>
+                          </DialogHeader>
+                          <div className="mt-4">
+                            {/* eslint-disable-next-line @next/next/no-img-element */}
+                            <img
+                              src={dish.image || "/images/default.jpg"}
+                              alt={dish.name}
+                              className="w-full h-auto rounded-md mb-4"
+                            />
+                            <p className="text-gray-700">
+                              Price:{" "}
+                              <span className="text-yellow-500">
+                                {dish.price}
+                              </span>
+                            </p>
+                          </div>
+                        </DialogContent>
+                      </Dialog>
+                    ))}
+                </td>
+                <td className={style.btnContainer}>
+                  <div className={style.btnTableDiv}>
+                    <button
+                      className={`${style.btn} ${style.btnTable}`}
+                      onClick={() => handleUpdate(menu)}
+                    >
+                      Update
+                    </button>
+                    <button
+                      className={`${style.btn} ${style.btnTable}`}
+                      onClick={() => handleDelete(menu)}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
