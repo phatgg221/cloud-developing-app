dcc3708c41d4cfeb5dc806e381da0559045dac8e
phatgg221 <129394719+phatgg221@users.noreply.github.com>
2025-01-12 18:37:57 +0700

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,7 @@
 
-import Header from '../components/application_component/Header';
-import Hero from '@/components/application_component/Hero';
+import Hero from '../components/application_component/Hero';
 import React from 'react';
-import IntroductionComponent from '@/components/application_component/IntroductionComponent';
+import IntroductionComponent from '../components/application_component/IntroductionComponent';
 export default function Home() {
   return (
     <div>
